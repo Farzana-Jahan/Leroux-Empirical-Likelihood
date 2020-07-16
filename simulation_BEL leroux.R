507f8c7c6a6f96a6ee944f6dc0e77e0513b7c543
Farzana Jahan <32354089+Farzana-Jahan@users.noreply.github.com>
2020-07-16 13:14:49 +1000

    Update simulation_BEL leroux.R
    New simulation study generation

--- simulation_BEL leroux.R
+++ simulation_BEL leroux.R
@@ -1,41 +1,49 @@
-# .libPaths("c:/software/Rpackages")
-# setwd("C:/R dir")
-## generate some covariates 
-library(emplik)
-x1<- runif(2147)
-#x2<-rbinom(2147,1,0.5)
-psi_true<-rnorm(2147)
-beta_true<- c(7,14)
+# creating a autoregression matrix randomly to model BYM and leroux 
+
+# creating autoregression matrix randomly for a set of 50 small areas
+
+R<- matrix(sample(-1:0,2500,replace=T),nrow=50, ncol=50)
+diag_R<-c()
+for(i in 1:50){
+  diag_R[i]<- sum(R[i,]== -1)
+}
+
+diag(R)<- diag_R
+dim(R)
+
+# taking true rho value as 0, independent case, generate spatial random effect psi
+n=50
+rho_true<- 0
+D_g_inv<-(1-rho_true)*diag(n)+rho_true*R
+sigma_true<- 0.2
+D<-solve(D_g_inv)
+set.seed(100)
+psi_true<- mvrnorm(1,rep(0,n),Sigma=D*sigma_true)
+x1<-rnorm(50)
 x<- cbind(1,x1)
+beta_true<- c(5,12)
 xbeta_true<- x%*% beta_true
-hist(xbeta_true)
-summary(xbeta_true)
+y<- xbeta_true+psi_true
 # generating response variable
-#y<-exp(xbeta_true+psi_true)
-y<-(xbeta_true+psi_true)
+y<-exp(xbeta_true+psi_true)
+y<-round(y)
+
+# taking true rho value as 0.75, moderate to high autocorrelation case, generate spatial random effect psi
+n=50
+rho_true<- 0.75
+D_g_inv<-(1-rho_true)*diag(n)+rho_true*R
+sigma_true<-1
+D<-solve(D_g_inv)
+# install.packages("corpcor")
+library(corpcor)
+set.seed(100)
+psi_true<- mvrnorm(1,rep(0,n),Sigma= make.positive.definite(D*sigma_true))
+x1<-rnorm(50)
+x<- cbind(1,x1)
+beta_true<- c(5,12)
+xbeta_true<- x%*% beta_true
+y<- xbeta_true+psi_true
 y<-round(y)
 
 
-
-
-# reading spatial data
-##############################
-library(tidyverse)
-#install.packages("spdep")
-library(spdep)
-aus_nb<-read.gal("SA2_2011_AUST_qv7.gal", override.id = TRUE)
-class(aus_nb)
-W<-nb2mat(aus_nb,style="B")
-nblist<-nb2listw(aus_nb)
-#creating symmetric neighbourhood matrix for BYM in CARBAYES
-#rownames(W)<-c()
-#ind <- upper.tri(W)
-#W[ind] <- t(W)[ind] 
-#isSymmetric(W)
-ni<-rowSums(W) # no. of neighbours for each area
-R<-diag(ni)
-for(i in 1:nrow(R))
-{
-  R[i,which(W[i,]==1)]<- -1
-}
 
